25b61575055cc45802d5f4ceac3c5831ed2741d9
Erick Arias <erickarias1999@hotmail.com>
2022-01-13 10:07:30 -0500

    Java

---
+++ Java/src/E1_WelcomeToJava.java
@@ -0,0 +1,9 @@
+public class E1_WelcomeToJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    }
+}
